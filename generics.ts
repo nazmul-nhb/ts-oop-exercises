e0db1594af7df5ce6e601bf8ee00d3ec9771a602
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-08-27 23:08:11 +0600

    solve compile error

--- generics.ts
+++ generics.ts
@@ -6,3 +6,9 @@
 function echo<T>(arg: T): T {
 	return arg;
 }
+
+// When compiling the following piece of code, we get an error saying ‘Property name does not exist on type T’. How can we solve this problem?
+
+function printName<T extends { name: string }>(obj: T) {
+	console.log(obj.name);
+}
